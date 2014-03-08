446af53b68efdb54de47d6ef3a866d491ea2cf7b
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-03-08 21:59:27 +0100

    Fix bug: EPEL will not download repo file

--- rd_installer.sh
+++ rd_installer.sh
@@ -71,7 +71,7 @@ elif [[ ${OS_VERSION} -eq 5 ]]; then
 	rpm -Uh --quiet http://dl.fedoraproject.org/pub/epel/${OS_VERSION}/${ARCH_TYPE}/epel-release-5-4.noarch.rpm > /dev/null 2>&1
 	rpm -Uh --quiet http://poptop.sourceforge.net/yum/stable/rhel5/pptp-release-current.noarch.rpm > /dev/null 2>&1
 else
-	continue
+	echo ""
 fi
 
 # Install required packages
