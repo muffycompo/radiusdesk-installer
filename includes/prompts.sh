1966472257be00786169ef2cfb391cf333a25c0f
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-03-12 14:46:07 +0100

    Improve Output

--- includes/prompts.sh
+++ includes/prompts.sh
@@ -3,29 +3,34 @@
 ########## Start Prompts #########
 
 function ask_for_webserver(){
-	echo -e "\n"
+	echo ""
 	read -p "What web server should we use? [N]ginx or [A]pache: " answer
 	case "${answer}" in 
-	  n|N|nginx|Nginx ) 
+	  n|N|nginx|Nginx )
+		echo ""
 		echo -e "Using ${LIGHT_BLUE}${BOLD}Nginx${F_END} Web server"
 		webserver="nginx";;
-	  a|A|apache|Apache ) 
+	  a|A|apache|Apache )
+		echo ""
 		echo -e "Using ${LIGHT_BLUE}${BOLD}Apache${F_END} Web server"
 		webserver="httpd";;
-	  * ) 
-		  echo -e "${LIGHT_RED}Oops...something went wrong, perharps you made a mistake in you selection!${F_END}"
-		  exit 1;;
+	  * )
+		echo ""
+		echo -e "${LIGHT_RED}Oops...something went wrong, perharps you made a mistake in you selection!${F_END}"
+		exit 1;;
 	esac
 }
 
 function ask_for_reboot(){
-	echo -e "\n"
+	echo ""
 	read -p "Do you want to reboot your computer now? [Y]es or [N]o: " answer
 	case "${answer}" in 
-	  y|Y|yes|Yes ) 
+	  y|Y|yes|Yes )
+		echo ""
 		echo -e "${LIGHT_RED}${BOLD}Rebooting...${F_END}will be back in second :)"
 		init 6;;
-	  n|N|no|No ) 
+	  n|N|no|No )
+		echo ""
 		echo -e "No biggy...we will do it another time :)";;
 	  * ) 
 		exit 1;;
