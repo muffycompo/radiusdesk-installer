6a950b0a5d7895951f45c21f35840f79f5fe0001
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-03-12 13:25:32 +0100

    Add logic to detect script path

--- includes/functions.sh
+++ includes/functions.sh
@@ -211,7 +211,7 @@ function configure_radiusdesk_freeradius(){
 	sed -i 's|"/usr/local/bin/radclient"|"/usr/bin/radclient"|g' ${1}cake2/rd_cake/Setup/Scripts/radscenario.pl
 
 	ln -s ${2}sites-available/dynamic-clients ${2}sites-enabled/dynamic-clients
-	cat ${OLDPWD}/utils/dynamic-clients > ${2}sites-enabled/dynamic-clients
+	cp -aR ${3}dynamic-clients > ${2}sites-enabled/dynamic-clients
 
 cat > ${2}modules/raw <<EOF
 raw { 
@@ -251,6 +251,8 @@ function fix_radiusdesk_permissions_ownership(){
 # Create Temporary Directory
 function mk_temp_dir(){
 	mkdir -p /tmp/radiusdesk/
+	# Temporary fix
+	cp -aR utils/dynamic-clients /tmp/radiusdesk/
 }
 
 # Clear Temporary Directory
